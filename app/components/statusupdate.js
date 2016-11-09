c6317ef948ba1347c03ea06fd6f27a5a3c951e37
Gahyun (Susie) Kim <gahyunkim@umass.edu>
2016-11-09 03:20:17 -0500

    fb7

--- app/components/statusupdate.js
+++ app/components/statusupdate.js
@@ -1,32 +1,35 @@
 import React from 'react';
 import {unixTimeToString} from '../util'
+import {Link} from 'react-router';
 
 export default class StatusUpdate extends React.Component {
   render() {
     return (
       <div>
-      <div className="row">
-      <div className="col-md-10">
-      <div className="media">
-      <div className="media-left media-top">
-      PIC
-      </div>
-      <div className="media-body">
-      <a href="#">{this.props.author.fullName}</a>
-      <br /> {unixTimeToString(this.props.postDate)} · {this.props.location} · <span
-      className="glyphicon glyphicon-user"></span>
-      </div>
-      </div>
-      </div>
-      <div className="col-md-2">
-      <span className="caret pull-right"></span>
-      </div>
-      </div>
-      <div className="row">
-      <div className="col-md-12">
-      {this.props.children}
-      </div>
-      </div>
+        <div className="row">
+          <div className="col-md-10">
+            <div className="media">
+              <div className="media-left media-top">
+                PIC
+              </div>
+              <div className="media-body">
+                <Link to={"/profile/" + this.props.author._id}>
+                  {this.props.author.fullName}
+                </Link>
+                <br /> {unixTimeToString(this.props.postDate)} · {this.props.location} · <span
+                  className="glyphicon glyphicon-user"></span>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-2">
+            <span className="caret pull-right"></span>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-12">
+            {this.props.children}
+          </div>
+        </div>
       </div>
     )
   }
