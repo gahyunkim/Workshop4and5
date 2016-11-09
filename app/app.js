857fded6cf4a96ac9c001607acf61c5eec44860e
Gahyun (Susie) Kim <gahyunkim@umass.edu>
2016-11-08 22:43:52 -0500

    fb5

--- app/app.js
+++ app/app.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Feed from './components/feed';
-import { getFeedData } from './server';
 
 ReactDOM.render(
-	<Feed />,
+	<Feed user={4} />,
 	document.getElementById('fb-feed')
 );
